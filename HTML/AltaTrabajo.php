8ad67272a3019fc188f5e8b80fb85061c72df8f3
santiagolopezbazarra <santiagolbazarra@gmail.com>
2024-09-12 12:11:03 +0200

    Arreglo desplegables AltaTrabajo.php + conexión con tablas obra y maquinaria

--- HTML/AltaTrabajo.php
+++ HTML/AltaTrabajo.php
@@ -3,11 +3,17 @@
         
     $conex = connection();
     
-    $sql = "SELECT * FROM obra";
+    $sqlObra = "SELECT * FROM obra WHERE OB_ACTIVA = 1";
+    $sqlMaquinaria = "SELECT * FROM maquinaria WHERE MA_ACTIVA = 1";
 
     try {
-        $stmt = $conex->query($sql);
-        $empresas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //ejecución sql sobre tabla obra
+        $stmtObra = $conex->query($sqlObra);
+        $obras = $stmtObra->fetchAll(PDO::FETCH_ASSOC);
+
+        //ejecución sql sobre tabla maquinaria        
+        $stmtMaquinaria = $conex->query($sqlMaquinaria);
+        $maquinarias = $stmtMaquinaria->fetchAll((PDO::FETCH_ASSOC));
     } catch (PDOException $e) {
         echo "Error al ejecutar la consulta: " . $e->getMessage();
         exit();
@@ -24,19 +30,26 @@
 <body onload="cargarDia()">
     <h1 id="elementoFecha"></h1>
     <select name="obras" id="obras">
+        <option value="" disabled selected>Selecciona una obra</option>
         <?php
-          // Recorre el array $empresas obtenido desde la base de datos
-          foreach($empresas as $empresa) {
-            // Para cada empresa, crea una opción con el nombre de la razón social
-            echo "<option value='".$empresa['OB_NOMBRE']."'>";
-          }
+        // Recorremos el array $obras
+        foreach($obras as $obra) {
+            // Para cada obra, generamos una opción con el nombre visible entre las etiquetas option
+            echo "<option value='" . htmlspecialchars($obra['OB_NOMBRE']) . "'>" . htmlspecialchars($obra['OB_NOMBRE']) . "</option>";
+        }
         ?>
     </select>
     <input type="time" name="horaEntrada" id="horaEntrada">
     <input type="time" name="horaSalida" id="horaSalida">
     <select name="maquinas" id="maquinas">
-        <option value="MQ-01">maquina 1</option>
-        <option value="MQ-02">maquina 2</option>
+        <option value="" disabled selected>Selecciona una maquina</option>
+        <?php
+        // Recorremos el array $obras
+        foreach($maquinarias as $maquinaria) {
+            // Para cada obra, generamos una opción con el nombre visible entre las etiquetas option
+            echo "<option value='" . htmlspecialchars($maquinaria['MA_NOMBRE']) . "'>" . htmlspecialchars($maquinaria['MA_NOMBRE']) . "</option>";
+        }
+        ?>
     </select>
     <button class="observaciones">OBSERVACIONES</button>
     <button class="enviar">ENVIAR</button>
